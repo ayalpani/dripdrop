195013109b633c58ab4778b363bfd6c5fede5cf2
“Maria <maria@eyalpani.de>
2021-10-21 16:25:34 +0200

    workflow

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: "jsdom",
+    testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(test).ts?(x)"],
     setupFilesAfterEnv: ['<rootDir>/jest-setup.ts']
 }
