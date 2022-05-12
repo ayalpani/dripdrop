ef011dc1a5a953dda4b9f5d11d99faf5c06f6ca1
Arash Yalpani <arash@yalpani.de>
2022-05-12 23:01:29 +0200

    index.d.ts....

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,11 @@
 export declare type QueryCallbackType = () => void;
 export declare function useSubscribe(subscriptionId: string): void;
 export declare function notifySubscribers(subscriptionId: string): void;
+export declare function registerCallback(
+  subscriptionId: string,
+  cb: QueryCallbackType
+): void;
+export declare function unregisterCallback(
+  subscriptionId: string,
+  cb: QueryCallbackType
+): void;
