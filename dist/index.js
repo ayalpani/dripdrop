679582c7555ba2cbae6674ef2c3f7ab9f13b5dfe
Arash Yalpani <arash@yalpani.de>
2022-09-13 11:39:07 +0200

    forgot to build dist...

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.notifySubscribers = exports.useSubscribe = exports.unregisterCallback = exports.registerCallback = exports.getSubscriptionCallbacksForTesting = void 0;
+exports.getSubscriptionCallbacksForTesting = exports.ivity = exports.notifySubscribers = exports.useSubscribe = exports.unregisterCallback = exports.registerCallback = void 0;
 const react_1 = require("react");
+// Center piece of react-ivity: a map of all registered subscription callbacks
 const subscriptionCallbacks = {};
-// exported for testing only
-const getSubscriptionCallbacksForTesting = () => {
-    return { ...subscriptionCallbacks };
-};
-exports.getSubscriptionCallbacksForTesting = getSubscriptionCallbacksForTesting;
+// Used by useSubscribe react hook on mount. Can be called outside of react
 function registerCallback(subscriptionId, callback) {
     if (!subscriptionCallbacks[subscriptionId]) {
         subscriptionCallbacks[subscriptionId] = [];
@@ -15,16 +12,20 @@ function registerCallback(subscriptionId, callback) {
     subscriptionCallbacks[subscriptionId].push(callback);
 }
 exports.registerCallback = registerCallback;
+// Used by useSubscribe react hook on unmount. Can be called outside of react
 function unregisterCallback(subscriptionId, callback) {
     var _a;
     subscriptionCallbacks[subscriptionId] = (_a = subscriptionCallbacks[subscriptionId]) === null || _a === void 0 ? void 0 : _a.filter((cb) => cb !== callback);
 }
 exports.unregisterCallback = unregisterCallback;
+// React Trigger Callback Hook
 function useTriggerCallbackRef() {
-    // define trigger function to rerender component that uses the useSubscribe* hooks
+    // define trigger to re-render components that uses the useSubscribe* hooks
     const [, setTrigger] = (0, react_1.useState)(0);
     return (0, react_1.useRef)(() => setTrigger((trigger) => trigger + 1)).current;
 }
+// Subscribes to value and triggers React Component re-render when
+// a value change is notified
 function useSubscribe(subscriptionId) {
     const callback = useTriggerCallbackRef();
     // register and unregister trigger in registry
@@ -34,11 +35,24 @@ function useSubscribe(subscriptionId) {
     }, [subscriptionId, callback]);
 }
 exports.useSubscribe = useSubscribe;
+// Call this after you have updated a value and want subscribers to know
 function notifySubscribers(subscriptionId) {
     var _a;
     (_a = subscriptionCallbacks[subscriptionId]) === null || _a === void 0 ? void 0 : _a.forEach((cb) => cb());
 }
 exports.notifySubscribers = notifySubscribers;
+// for more explicit use of the API
+exports.ivity = {
+    registerCallback,
+    unregisterCallback,
+    useSubscribe,
+    notifySubscribers,
+};
+// exported for testing only
+const getSubscriptionCallbacksForTesting = () => {
+    return { ...subscriptionCallbacks };
+};
+exports.getSubscriptionCallbacksForTesting = getSubscriptionCallbacksForTesting;
 // attach an api object to window: window.__react_ivity
 window.__react_ivity = {
     getSubscriptionCallbacks: () => {
