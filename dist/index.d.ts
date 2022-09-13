25082d85281b995c349deb4fd51e3b3c4f1fee24
Arash Yalpani <arash@yalpani.de>
2022-09-13 12:06:37 +0200

    fixed type declarations generation

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,15 @@
-export declare type QueryCallbackType = () => void;
+declare type $QueryCallbackType = () => void;
+export declare function registerCallback(subscriptionId: string, callback: $QueryCallbackType): void;
+export declare function unregisterCallback(subscriptionId: string, callback: $QueryCallbackType): void;
 export declare function useSubscribe(subscriptionId: string): void;
 export declare function notifySubscribers(subscriptionId: string): void;
-export declare function registerCallback(
-  subscriptionId: string,
-  cb: QueryCallbackType
-): void;
-export declare function unregisterCallback(
-  subscriptionId: string,
-  cb: QueryCallbackType
-): void;
+export declare const ivity: {
+    registerCallback: typeof registerCallback;
+    unregisterCallback: typeof unregisterCallback;
+    useSubscribe: typeof useSubscribe;
+    notifySubscribers: typeof notifySubscribers;
+};
+export declare const getSubscriptionCallbacksForTesting: () => {
+    [x: string]: $QueryCallbackType[];
+};
+export {};
